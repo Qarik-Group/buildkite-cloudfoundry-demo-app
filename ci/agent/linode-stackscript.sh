47e98a87a29c55dc628dcb4474f0a698ae622fe8
Dr Nic Williams <drnicwilliams@gmail.com>
2020-02-23 18:44:37 +1000

    add linode agent startup scripts

---
+++ ci/agent/linode-stackscript.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# This script is the StackScript for Linode
+
+exec >/var/log/stackscript.log 2>&1
+
+set -eux
+
+# <UDF name="buildkite_token" Label="Buildkite account token" />
+
+apk add docker bash git
+
+rc-update add docker boot
+service docker start
+
+TOKEN="$BUILDKITE_TOKEN" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
+
+BUILDKITE_DIR=/root/.buildkite-agent
+
+export BUILDKITE_AGENT_NAME="linode-$LINODE_ID-dc-$LINODE_DATACENTERID"
+sed -i "s/name=.*$/name=\"$BUILDKITE_AGENT_NAME\"/g" $BUILDKITE_DIR/buildkite-agent.cfg
+
+# Create buildkite user
+adduser -G docker -u 100000 -D buildkite
+
+# TODO: create /etc/init.d/buildkite-agent
+# TODO: rc-update add buildkite-agent
+# TODO: service buildkite-agent start
