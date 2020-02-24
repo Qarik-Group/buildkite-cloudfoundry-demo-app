7ba5251839ac4b7b100deeba20f76c2e0724a548
Dr Nic Williams <drnicwilliams@gmail.com>
2020-02-24 16:24:49 +1000

    if only $CF_SPACE & $CF_ROUTE provided, then single deployment

--- ci/scripts/build-pipeline-from-env-vars.sh
+++ ci/scripts/build-pipeline-from-env-vars.sh
@@ -2,10 +2,30 @@
 
 set -eu
 
-env | sort >&2
-
-cat <<YAML
+if [[ -n "${CF_ROUTE:-}" && -n "${CF_SPACE:-}" ]]; then
+  echo "Configuring for single deployment" >&2
+  cat <<YAML
+steps:
+  - label: ":cloudfoundry:"
+    command: "ci/scripts/cf-push-or-action.sh"
+    artifact_paths: "."
+    concurrency: 1
+    concurrency_group: "cf-push"
+    plugins:
+      - docker#v3.5.0:
+          image: "starkandwayne/update-all-cf-buildpacks:latest"
+          propagate-environment: true
+    timeout_in_minutes: 5
+    env:
+      CF_API: ""
+      CF_SKIP_SSL_VALIDATION: ""
+      CF_USERNAME: ""
+      CF_PASSWORD: ""
+YAML
+else
+  cat <<YAML
 steps:
 - block: "Deploy"
   prompt: "Deploy to production?"
 YAML
+fi
