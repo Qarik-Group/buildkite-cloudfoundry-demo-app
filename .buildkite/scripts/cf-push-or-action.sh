becc664c4504e48e67b635caccc23b5a448e7c3d
Dr Nic Williams <drnicwilliams@gmail.com>
2020-03-04 09:57:16 +1000

    set an annotation of deployed cf app towards an idea for an auto-reaper

--- .buildkite/scripts/cf-push-or-action.sh
+++ .buildkite/scripts/cf-push-or-action.sh
@@ -4,6 +4,8 @@
 
 set -eu
 
+: "${CF_APPNAME:?required}"
+: "${CF_MANIFEST:=manifest.yml}"
 : "${CF_ORGANIZATION:?required}"
 
 [[ "${CF_SPACE_SELECTOR:-X}" == "X" ]] || {
@@ -20,4 +22,14 @@ echo "+ cf target -o ${CF_ORGANIZATION} -s ${CF_SPACE}"
 cf target -o "${CF_ORGANIZATION}" -s "${CF_SPACE}"
 echo
 
-( set -x; cf push --var route="${CF_ROUTE}" )
+( set -x; cf push "$CF_APPNAME" -f "${CF_MANIFEST}" --var route="${CF_ROUTE}" )
+
+echo
+app_guid=$(cf app "$CF_APPNAME" --guid)
+# TODO: set reap-me-after annotation
+annotation=cf.starkandwayne.com/reap-me-after
+reap_timestamp="2020-03-04"
+echo "Setting annotation $annotation=$reap_timestamp"
+annotation_json=$(jq -rc --arg annotation "$annotation" --arg timestamp "$reap_timestamp" '.[$annotation] = $timestamp' <<< '{}')
+patch_json=$(jq -rc '{"metadata": {"annotations": .}}' <<< "$annotation_json")
+( set -x; cf curl "/v3/apps/$app_guid" -X PATCH -d "$patch_json")
