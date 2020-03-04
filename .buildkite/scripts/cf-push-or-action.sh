e19fec89ff0b16c97382d8988eeca2d326a35748
Dr Nic Williams <drnicwilliams@gmail.com>
2020-03-04 10:05:44 +1000

    simpler annotation label for git-commit

--- .buildkite/scripts/cf-push-or-action.sh
+++ .buildkite/scripts/cf-push-or-action.sh
@@ -28,12 +28,11 @@ echo
 APP_GUID="$(cf app "$CF_APPNAME" --guid)"
 APP_URI="/v3/apps/${APP_GUID}"
 
-# TODO: set reap-me-after annotation
 reap_timestamp="2020-03-04"
 commit_sha="$(git rev-parse --short HEAD)"
 
 annotation_json=$(jq -rc --arg annotation "cf.starkandwayne.com/reap-me-after" --arg value "$reap_timestamp" '.[$annotation] = $value' <<< '{}')
-annotation_json=$(jq -rc --arg annotation "cf.starkandwayne.com/git-commit" --arg value "$commit_sha" '.[$annotation] = $value' <<< "$annotation_json")
+annotation_json=$(jq -rc --arg annotation "git-commit" --arg value "$commit_sha" '.[$annotation] = $value' <<< "$annotation_json")
 
 patch_json=$(jq -rc '{"metadata": {"annotations": .}}' <<< "$annotation_json")
 ( set -x; cf curl "$APP_URI" -X PATCH -d "$patch_json")
